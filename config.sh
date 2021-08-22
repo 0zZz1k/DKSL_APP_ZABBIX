804b8ceed247c39b00fbf2512a1db3168f3b777c
Vadim <vv.lisyak@gmail.com>
2021-08-23 00:01:33 +0300

    test

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ apt install /tmp/iso/netping/zabbix/zabbix-release_5.2-1+ubuntu20.04_all.deb
 apt clean
 apt install --download-only -y zabbix-server-pgsql zabbix-frontend-php php7.4-pgsql zabbix-nginx-conf zabbix-agent
 cp /var/cache/apt/archives/*.deb ./iso/netping/deb/zabbix/
-cp ./zabbix-release_*_all.deb ./iso/netping/deb/zabbix/
+mv /tmp/iso/netping/zabbix/zabbix-release_*_all.deb /tmp/iso/netping/deb/zabbix/
 #postgresql 
 apt clean
 apt install --download-only -y postgresql
